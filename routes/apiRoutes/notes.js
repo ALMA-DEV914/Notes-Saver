3352c890473773204519bc1f7cc3b9da680be90e
ALMA_DEV914 <aungonalna58@gamil.com>
2021-11-25 11:16:19 -0800

    Add app and routes for the application

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,23 +1,82 @@
 const fs = require('fs');
-const path = require('path');
 
-function createNewAnimal(body, animalsArray) {
-    const animal = body;
-    animalsArray.push(animal);
+const generateUniqueId = require('generate-unique-id');
 
-    fs.writeFileSync(
-      path.join(__dirname, '../data/animals.json'),
-      JSON.stringify({ animalsArray }, null, 2)
-    );
-    return animal;
-  }
 
-  function findById(id, animalsArray) {
-    const result = animalsArray.filter(animal => animal.id === id)[0];
-  
-  fs.writeFileSync(
-      path.join(__dirname, '../data/animals.json'),
-      JSON.stringify({ animalsArray }, null, 2)
-    );
-    return result;
-  }
+const editNote = (updatedNotesArray) => {
+  fs.writeFile('./db/db.json', JSON.stringify(updatedNotesArray), (err) => {
+     if(err) throw err;
+  });
+}
+
+module.exports = (app) => {
+
+app.get('/api/notes', (req, res) => {
+  fs.readFile(`.db/db.json`, 'utf8', (err, data) => {
+    if(err){
+      console.log (err)
+    } else {
+      const parsedNotes = JSON.parse(data);
+      res.json(parsedNotes);
+    }
+  });
+});
+
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if(err) throw err;
+
+    const notesArray = JSON.parse(data);
+    newNote.id = uniqueId({length: 28});
+    notesArray.push(newNote);
+
+    editNote(notesArray);
+    console.log(`New note added! Title: ${JSON.stringify(newNote.title)}, Text: ${JSON.stringify(newNote.text)}, Id: ${JSON.stringify(newNote.id)} `);
+  });
+  res.send(notesArray);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const deleteNotesById = req.param.id;
+  fs.readFile('./db/db.json', 'utf8', (req, res) => {
+    if(err) throw err;
+
+    let notesArray = JSON.parse(data);
+    for(let i = 0; i < notesArray.length; i++){
+      if(notesArray[i].id === deleteNotesById){
+        notesArray.splice(i, 1);
+      }
+    }
+    editNote(notesArray);
+    console.log(`Note Id: ${deleteNotesById} was deleted!`);
+    res.send(notesArray);
+  });
+});
+
+app.put('/api/notes/:id', (req, res) => {
+  const editNoteId = req.param.id;
+
+  fs.readFile('./db/db.json', (req, res) => {
+     if(err) throw err;
+
+     let notesArray = JSON.parse(data);
+     let selectedNote = notesArray.find((note) => note.id === editNoteId);
+       if(selectedNote){
+         let updateNote = {
+          title: req.body.title,
+          text: req.body.text,
+          id: req.body.id
+         };
+         let targetNoteIndex = notesArray.indexOf(selectedNote);
+         notesArray.splice(targetNoteIndex, 1, updateNote);
+
+         res.sendStatus(200);
+         editNoteId(notesArray);
+         res.json(notesArray);
+       } else {
+         res.sendStatus(404);
+       }
+  });
+});
+};
--- server.js
+++ server.js
@@ -2,18 +2,17 @@
 const express = require('express');
 // assign the url path/port
 const PORT = process.env.PORT || 3001;
+
 const app = express();
-// call the routes for the files
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+
 // use middleware for the root of the file path
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// Use apiRoutes
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+// call the routes for the files
+ require('./routes/apiRoutes/notes')(app);
+ require('./routes/htmlRoutes/index')(app);
 // calling the port to functions
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
